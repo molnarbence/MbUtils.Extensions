dfa4d6bc5079a329cfa4700a4bbb7fd99e2b6e89
Bence Molnár <xjarekx@gmail.com>
2025-04-28 16:03:08 +0200

    Update packages

---
+++ test/TestWorkerService/FooConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace TestWorkerService
+{
+   public class FooConfig
+   {
+      public const string SectionName = "Foo";
+      
+      public string? Bar { get; set; }
+   }
+}
--- test/TestWorkerService/Program.cs
+++ test/TestWorkerService/Program.cs
@@ -1,7 +1,10 @@
+using MbUtils.Extensions.Tools;
 using TestWorkerService;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<Worker>();
+
+builder.AddConfig<FooConfig>(FooConfig.SectionName);
 
 var host = builder.Build();
 host.Run();
