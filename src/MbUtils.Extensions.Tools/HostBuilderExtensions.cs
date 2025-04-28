8bb4149eb979e07ee8ece8e0afc4cf157a4540d0
Bence Molnár <xjarekx@gmail.com>
2025-04-28 15:56:56 +0200

    Release MbUtils.Extensions.Tools

---
+++ src/MbUtils.Extensions.Tools/HostBuilderExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace MbUtils.Extensions.Tools;
+
+public static class HostBuilderExtensions
+{
+   public static IHostApplicationBuilder AddConfig<TConfig>(this IHostApplicationBuilder builder, string configSectionName) where TConfig : class, new()
+   {
+      builder.Services.AddOptions<TConfig>()
+         .Bind(builder.Configuration.GetSection(configSectionName))
+         .ValidateDataAnnotations()
+         .ValidateOnStart();
+      
+      return builder;
+   }
+
+   public static IHostApplicationBuilder AddHomeFolderConfigurationFile(this IHostApplicationBuilder hostBuilder,
+      string folderName)
+   {
+      hostBuilder.Configuration.AddJsonFile(
+         GetHomeFolderConfigurationFile(folderName),
+         optional: true);
+         
+      return hostBuilder;
+   }
+   
+   private static string GetHomeFolderConfigurationFile(string folderName)
+   {
+      var homeFolder = Environment.GetEnvironmentVariable("HOME")
+                       ?? Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+      return Path.Combine(homeFolder, folderName, "appsettings.json");
+   }
+}
